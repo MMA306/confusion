49944127af2b7b8666d2168925285b054a1cc60a
Mahmoud Morsy <mdmorsi3@gmail.com>
2021-05-27 12:44:30 +1000

    Combining Reducers

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import { COMMENTS } from '../shared/comments'
-import { DISHES } from '../shared/dishes'
-import { LEADERS } from '../shared/leaders'
-import { PROMOTIONS } from '../shared/promotions'
+
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import About from './AboutComponent';
@@ -10,78 +7,86 @@ import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+
+
+const mapStateToProps = state => {
+
+    return {
+        comments: state.comments,
+        dishes: state.dishes,
+        leaders: state.leaders,
+        promotions: state.promotions,
+    }
+}
+
 
 class Main extends Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            comments: COMMENTS,
-            dishes: DISHES,
-            leaders: LEADERS,
-            promotions: PROMOTIONS,
-        };
-
     }
 
     render() {
-
-        const mapStateToProps = state => {
-            return {
-                dishes: state.dishes,
-                comments: state.comments,
-                promotions: state.promotions,
-                leaders: state.leaders
-            }
-        }
 
         const HomePage = () => {
             return (
                 <Home
                     dish={this.props.dishes.filter((dish) => dish.featured)[0]}
-                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                    promotion={this.props.promotions.filter((promotion) => promotion.featured)[0]}
                     leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
-        }
+        };
 
         const AboutUsPage = () => {
             return (
-                <About leaders={this.state.leaders} />
+                <About
+                    leaders={this.props.leaders}
+                />
             );
         };
 
 
         const DishWithId = ({ match }) => {
             return (
-                <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+                <DishDetail
+
+                    dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+
+
+                />
             );
-        }
+        };
 
 
 
         return (
             <div>
-                <Header />
-                <div>
-                    <Switch>
-                        <Route path='/home' component={HomePage} />
-                        <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
-                        <Route path='/menu/:dishId' component={DishWithId} />
-                        <Route exact path='/contactus' component={Contact} />} />
-              <Redirect to="/home" />
-                    </Switch>
-                </div>
-                <Footer />
+                <Header></Header>
+
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
+
+                    <Route path="/menu/:dishId" component={DishWithId} />
+
+                    <Route exact path="/contactus" component={Contact} />
+                    <Route exact path="/aboutus" component={AboutUsPage} />
+
+                    {/* if url dosesnt match, bydefault redirect to */}
+                    <Redirect to="/home" />
+                </Switch>
+
+                <Footer></Footer>
             </div>
         );
+
     }
+
 }
 
 export default withRouter(connect(mapStateToProps)(Main));
-
